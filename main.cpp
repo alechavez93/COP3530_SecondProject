bb02dd82d2bece643f90751b4556f83f2ad98500
Alejandro Chavez <alechavez93@gmail.com>
2016-04-18 17:25:33 -0400

    Initial structure with basic utility functions

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<map>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+//Realm => R
+int getMaxChanges(string R1, string R2);
+int getMaxIncantation(string R);
+
+int main() {
+
+	system("pause");
+	return 0;
+}
