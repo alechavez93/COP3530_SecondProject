acd86141074b2f80533c57dc7f0ea50b65eb2718
Alejandro Chavez <alechavez93@gmail.com>
2016-04-18 18:33:21 -0400

    main now gets all the nput and stores it in an unordered map of pairs (charm, listOfMagi), can comment the code that verifies getting correct input

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
-#include<map>
+#include<unordered_map>
 #include<string>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -10,6 +11,44 @@ int getMaxChanges(string R1, string R2);
 int getMaxIncantation(string R);
 
 int main() {
+
+	//N -> number of realms    M -> number of magi   P -> power of magi
+	int N, M, P;	string charm;
+	vector<int> listOfMagi;
+
+
+	//unordered map to save the whole input
+	unordered_map<string, vector<int>> Realms;
+	unordered_map < string, vector<int>>::iterator it;
+
+
+	//Getting input
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+
+		cin >> charm; cin >> M;
+
+		//Filling the vector with the magi's powers
+		for (int j = 0; j < M; j++) {
+			cin >> P;
+			listOfMagi.push_back(P);
+		}
+
+		//Creating a Realm as a pair
+		Realms.insert(make_pair(charm, listOfMagi));
+		listOfMagi.clear();
+	}
+
+	//Check input was collected correctly
+	for (it = Realms.begin(); it != Realms.end(); it++) {
+		cout << "Charm: " << it->first << endl;
+
+		for (int i = 0; i < it->second.size(); i++) {
+			cout << it->second[i] << " ";
+		}
+		cout << endl << endl;
+	}
+
 
 	system("pause");
 	return 0;
